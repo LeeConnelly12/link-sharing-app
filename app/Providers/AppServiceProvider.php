a366aa285800a2eee5de7773a0ef7aa20e9e1c6a
Lee Connelly <lee@leeconnelly.dev>
2023-07-06 00:43:02 +0100

    Setup app provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,9 @@
 
 namespace App\Providers;
 
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Http\Resources\Json\JsonResource;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +21,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Model::unguard();
+        Model::preventLazyLoading(!$this->app->isProduction());
+        JsonResource::withoutWrapping();
     }
 }
