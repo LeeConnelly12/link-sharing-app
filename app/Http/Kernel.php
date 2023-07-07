d0e59c08f26fb8c58b4f8ebbe7b112ae32e68349
Lee Connelly <lee@leeconnelly.dev>
2023-07-07 22:20:25 +0100

    Remove unused middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -61,7 +61,6 @@ class Kernel extends HttpKernel
         'cache.headers' => \Illuminate\Http\Middleware\SetCacheHeaders::class,
         'can' => \Illuminate\Auth\Middleware\Authorize::class,
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
-        'password.confirm' => \Illuminate\Auth\Middleware\RequirePassword::class,
         'precognitive' => \Illuminate\Foundation\Http\Middleware\HandlePrecognitiveRequests::class,
         'signed' => \App\Http\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
