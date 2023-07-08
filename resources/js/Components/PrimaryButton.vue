bb7b167ca722445b0d25a86373301de22fcabfc6
Lee Connelly <lee@leeconnelly.dev>
2023-07-08 02:21:01 +0100

    Disable button while loading

--- resources/js/Components/PrimaryButton.vue
+++ resources/js/Components/PrimaryButton.vue
@@ -15,6 +15,7 @@ defineProps({
 <template>
   <button
     :type="type"
+    :disabled="loading"
     class="inline-flex h-[46px] w-full items-center justify-center rounded-lg border border-transparent bg-purple px-4 font-semibold text-white transition duration-150 ease-in-out hover:bg-purple-hover-color focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 active:bg-purple-hover-color disabled:bg-purple-hover-color"
   >
     <div v-if="loading" class="btn-spinner"></div>
