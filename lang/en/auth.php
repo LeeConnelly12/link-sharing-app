f1d33f6ccec82d3e69fef447e00e9abd92e3c67c
Lee Connelly <lee@leeconnelly.dev>
2023-07-11 00:07:20 +0100

    Reduce error message length

--- lang/en/auth.php
+++ lang/en/auth.php
@@ -13,8 +13,8 @@ return [
     |
     */
 
-    'failed' => 'These credentials do not match our records.',
-    'password' => 'The provided password is incorrect.',
+    'failed' => 'Please check your credentials',
+    'password' => 'The password is incorrect.',
     'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
 
 ];
