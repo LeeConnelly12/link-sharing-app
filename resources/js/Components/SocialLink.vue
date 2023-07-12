70774eb73da6f09728a7b40de76c685b33ce2078
Lee Connelly <lee@leeconnelly.dev>
2023-07-12 02:15:30 +0100

    Unset social link alt

--- resources/js/Components/SocialLink.vue
+++ resources/js/Components/SocialLink.vue
@@ -21,7 +21,7 @@ defineProps({
       link.platform === 'Frontend Mentor' ? 'text-dark-gray' : 'text-white'
     "
   >
-    <img :src="getIconForPlatform(link.platform)" :alt="`${link.platform}'s logo'`">
+    <img :src="getIconForPlatform(link.platform)" alt="">
     <p>{{ link.platform }}</p>
     <svg width="16" height="16" fill="none" viewBox="0 0 16 16">
       <path
